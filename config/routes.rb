0fc1d58a6297242420f4f690b81f8092733624f9
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2024-12-09 01:02:25 +0900

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }
-  root 'static_pages#top'
+  root "static_pages#top"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :posts
 end
