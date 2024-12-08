b7ca4bda33504ed037da3a934189a34673305ff0
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2024-12-09 01:58:19 +0900

    top

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get "posts/index"
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }
   root 'static_pages#top'
+  resources :posts, only: [:index]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_05_121520) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_08_161036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "thumbnail"
+    t.string "user_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
