cad68ba6eae3130ed3c496c78abd303af250d6ca
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2024-12-13 02:16:39 +0900

    youtube

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :video_file
-  validates :video_url, format: { with: /\Ahttps:\/\/www\.youtube\.com\/watch\?v=.*\z/, message: "はYouTubeのURLである必要があります" }, allow_blank: true
 end
