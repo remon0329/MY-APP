42f2300b9a72af46d70d43a7d064da4fbab4c58a
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2024-12-15 04:51:39 +0900

    test

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:one) # ユーザーのファクトリを使うか、適切なユーザーを指定
+    sign_in @user # Deviseを使っている場合のサインイン
+  end
+
   test "should get show" do
-    get profiles_show_url
+    get profile_path(@user)
     assert_response :success
   end
 end
