416a84dc61d4115c93c8552c636b2c516c87a9d2
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2024-12-15 04:44:32 +0900

    profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "profiles/show"
   get "posts/index"
   devise_for :users, controllers: {
     registrations: "users/registrations"
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
     get "/users/sign_out" => "devise/sessions#destroy"
   end
   root "posts#top"
-  resources :posts, only: [ :index, :new, :create ]
+  resources :posts, only: [ :index, :new, :create, :show ]
+  resources :profiles, only: [ :show ]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
