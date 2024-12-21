0f64023ea338aa09c2aa4c3f0180af8e8125b629
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2024-12-22 03:59:55 +0900

    description

--- db/migrate/20241208161036_create_posts.rb
+++ db/migrate/20241208161036_create_posts.rb
@@ -5,8 +5,6 @@ class CreatePosts < ActiveRecord::Migration[7.2]
       t.text :body, null: false
       t.string :thumbnail
       t.string :user_name
-      t.string :video_url # 動画のURLを保存するカラムを追加
-      t.text :description # 説明欄を追加
 
       t.timestamps
     end
