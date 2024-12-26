3f8c3a20266d9210ce63d342b62d880bfc6b32c2
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2024-12-26 17:21:48 +0900

    lint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
-  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [ :create, :edit, :update, :destroy ]
+  before_action :set_post, only: [ :edit, :update, :destroy ]
 
   def index
     @posts = Post.all
