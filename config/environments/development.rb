0ee07b8ea9c4d9e2479143da0c5967827c226373
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2024-12-27 07:50:10 +0900

    mail_2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,25 +35,15 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener_web
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-
-  config.action_mailer.smtp_settings = {
-    enable_starttls_auto: true,
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "smtp.gmail.com",
-    user_name: "remon12273@gmail.com",
-    password: "danbo0239sena0329",
-    authentication: "login"
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "remon12273@gmail.com"
+  config.mailer_sender = "chimeiwang@example.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
