f44fd91c98de10ea3aba0e33fb3eb84e772afd5a
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2025-01-08 01:58:05 +0900

    validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_one_attached :video_file
+  validates :video_url_or_video_file, presence: { message: "YouTubeの動画URLまたは動画ファイルを入力してください" }
+
+  private
+
+  def video_url_or_video_file
+    if video_url.blank? && video_file.blank?
+      errors.add(:base, "YouTubeの動画URLまたは動画ファイルを入力してください")
+    end
+  end
 end
