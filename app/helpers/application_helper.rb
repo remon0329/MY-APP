075fbd9093f2f9d22e8f19afded33626c85f80b9
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2025-02-14 03:41:48 +0900

    画像表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: "GAMES_ MEMORY",
+      title: "GAMES_ MEMORY",
+      reverse: true,
+      charset: "utf-8",
+      description: "思い出深いゲームやハマったゲーム動画を埋め込んだり、スレッドを投稿してもらい
+      色々な世代の人気なゲームからマイナーなゲームまで気軽に共有できるサービスです。",
+      canonical: request.original_url,
+      separator: "|",
+      og: {
+          site_name: "GAMES_ MEMORY",
+          title: "GAMES_ MEMORY",
+        description: "思い出深いゲームやハマったゲーム動画を埋め込んだり、スレッドを投稿してもらい
+        色々な世代の人気なゲームからマイナーなゲームまで気軽に共有できるサービスです。",
+        type: "website",
+        url: request.original_url,
+        image: image_url("OGP.png"),
+        local: "ja-JP"
+      },
+
+      twitter: {
+        card: "summary_large_image",
+        site: "@https://x.com/remon_tanosimu",
+        image: image_url("OGP.png")
+      }
+    }
+  end
 end
