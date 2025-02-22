9cbb91292e10856936a44e9062575d2f9321715b
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2025-02-23 08:18:47 +0900

    アイコンの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable, :rememberable,
          :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.image = auth.info.image
     end
