49877f6caaf463e1b8a7947c33bb6beaf96c2ffc
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2025-03-18 01:30:47 +0900

    manifest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  def manifest
-    render json: {
-      name: "GAMES_MEMORY",
-      short_name: "GAMES_MEMORY",
-      start_url: "/",
-      display: "standalone",
-      background_color: "#ffffff",
-      theme_color: "#000000",
-      description: "思い出深いゲームやハマったゲーム動画を共有するサービス",
-      icons: [
-        {
-          src: "/icon.png",
-          sizes: "192x192",
-          type: "image/png"
-        },
-        {
-          src: "/icon.svg",
-          sizes: "any",
-          type: "image/svg+xml"
-        }
-      ]
-    }
-  end
 end
