facd3a6bf65c30ebbee7deb427c25cfe97c7a4e9
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2025-03-19 03:27:07 +0900

    ドロップダウンメニュー追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,12 +4,20 @@ import "./controllers"
 import Rails from "@rails/ujs";
 Rails.start();
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('turbo:load', function () {
   const dropdownButton = document.getElementById('dropdownMenuButton');
   if (dropdownButton) {
     const dropdownMenu = document.getElementById('dropdownMenu');
     dropdownButton.addEventListener('click', function () {
-    dropdownMenu.classList.toggle('hidden');
+      dropdownMenu.classList.toggle('hidden');
+    });
+
+    // ドロップダウン外をクリックした場合にメニューを閉じる
+    document.addEventListener('click', function (e) {
+      // ドロップダウンボタン外をクリックした場合にメニューを閉じる
+      if (!dropdownButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
+        dropdownMenu.classList.add('hidden');
+      }
     });
   }
 });
