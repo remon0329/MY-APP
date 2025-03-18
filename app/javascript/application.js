5a0ffec5874b454237d82cafd3f38ce21f4abfa2
山本隼秀 <yamamotosihide@yamamotoshunshuunoMacBook-Air.local>
2025-03-19 02:17:50 +0900

    スマホのレスポンシブデザイン

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import Rails from "@rails/ujs";
 Rails.start();
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdownButton = document.getElementById('dropdownMenuButton');
+  if (dropdownButton) {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownButton.addEventListener('click', function () {
+    dropdownMenu.classList.toggle('hidden');
+    });
+  }
+});
